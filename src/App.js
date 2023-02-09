10ffeb0f74b29a432d76824dba1be08316d89288
enesakdn <enesakdogan948@gmail.com>
2023-02-10 02:17:42 +0300

    FINISH

--- src/App.js
+++ src/App.js
@@ -5,18 +5,33 @@
 */
 
 // State hook u import edin
-import React from 'react';
+import React, { useState } from "react";
 
 // Gönderiler (çoğul!) ve AramaÇubuğu bileşenlerini import edin, çünkü bunlar App bileşeni içinde kullanılacak
 // sahteVeri'yi import edin
-import './App.css';
+import "./App.css";
+import Gönderiler from "./bileşenler/Gönderiler/Gönderiler";
+import AramaÇubuğu from "./bileşenler/AramaÇubuğu/AramaÇubuğu";
+import sahteVeri from "./sahte-veri.js";
 
 const App = () => {
+  const [gonderiler, setGonderiler] = useState(sahteVeri);
+  const [arama, setArama] = useState("");
+
   // Gönderi nesneleri dizisini tutmak için "gonderiler" adlı bir state oluşturun, **sahteVeri'yi yükleyin**.
   // Artık sahteVeri'ye ihtiyacınız olmayacak.
   // Arama çubuğunun çalışması için , arama kriterini tutacak başka bir state'e ihtiyacımız olacak.
-	
-  const gonderiyiBegen = gonderiID => {
+
+  const gonderiyiBegen = (gonderiID) => {
+    setGonderiler(
+      gonderiler.map((item) => {
+        if (item.id === gonderiID) {
+          item.likes += 1;
+        }
+        return item;
+      })
+    );
+
     /*
       Bu fonksiyon, belirli bir id ile gönderinin beğeni sayısını bir artırma amacına hizmet eder.
 
@@ -29,9 +44,17 @@ const App = () => {
         - aksi takdirde, sadece gönderi nesnesini değiştirmeden döndürün.
      */
   };
-
+  const aramaYap = (arama) => {
+    let filterArray = [];
+    filterArray = sahteVeri.filter((item) => {
+      item.username.includes(arama);
+    });
+    setGonderiler(filterArray);
+  };
   return (
-    <div className='App'>
+    <div className="App">
+      <AramaÇubuğu arama={arama} aramaYap={aramaYap} />
+      <Gönderiler gonderiyiBegen={gonderiyiBegen} gonderiler={gonderiler} />
       {/* AramaÇubuğu ve Gönderiler'i render etmesi için buraya ekleyin */}
       {/* Her bileşenin hangi proplara ihtiyaç duyduğunu kontrol edin, eğer ihtiyaç varsa ekleyin! */}
     </div>
